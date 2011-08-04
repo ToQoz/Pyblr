4c3e7db13605c2bd1e78ffc4f956f9c82637356d
toqoz <taka.ad1991@gmail.com>
2011-08-04 11:37:07 +0900

    やっぱりunittestじゃなくてnoseで書くことにした.

--- test_pyblr.py
+++ test_pyblr.py
@@ -1,18 +1,32 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
-import unittest
+from nose.tools import *
 import pyblr
 import oauth2 as oauth
 import json
 
-class TestPyblr(unittest.TestCase):
-    def assertNotRaise(self, excClass, callable_, *args, **kwargs):
+
+
+class TestPyblr:
+    def setUp(self):
+        self.api_key = 'consumer key'
+        self.consumer = oauth.Consumer(self.api_key, 'consumer secret')
+        self.access_token = oauth.Token('oauth token', 'oauth token secret')
+        self.client = pyblr.Pyblr(oauth.Client(self.consumer, self.access_token))
+        self.params = {
+            "name": "toqoz",
+            "sex": "male"
+        }
+        self.parsed_params = "name=toqoz&sex=male"
+
+    def assert_not_raise(self, excClass, callable_, *args, **kwargs):
         try:
             callable_(*args, **kwargs)
         except excClass:
             if hasattr(excClass,'__name__'): excName = excClass.__name__
             else: excName = str(excClass)
-            self.fail("unexpected exception raised: %s" % excName)
+            message = "Against the expectationm Raised %s" % (excClass)
+            raise AssertionError(message)
 
     def create_response(self, status, msg):
         result = (
@@ -27,28 +41,14 @@ class TestPyblr(unittest.TestCase):
         )
         return result
 
-    def setUp(self):
-        self.api_key = 'consumer key'
-        self.consumer = oauth.Consumer(self.api_key, 'consumer secret')
-        self.access_token = oauth.Token('oauth token', 'oauth token secret')
-        self.client = pyblr.Pyblr(oauth.Client(self.consumer, self.access_token))
-        self.params = {
-            "name": "toqoz",
-            "sex": "male"
-        }
-        self.parsed_params = "name=toqoz&sex=male"
-
-    def test_parse_params(self):
-        self.assertEqual(
+    def parse_params_test(self):
+        assert_equals(
             self.client.parse_params(self.params),
             self.parsed_params
         )
 
-    def test_APIError(self):
-        self.assertNotRaise(pyblr.APIError, lambda: self.client.parse_response(self.create_response('200', 'OK')))
-        self.assertNotRaise(pyblr.APIError, lambda: self.client.parse_response(self.create_response('300', 'Found')))
-        self.assertRaises(pyblr.APIError, lambda: self.client.parse_response(self.create_response('401', 'Not Authorized')))
-        self.assertRaises(pyblr.APIError, lambda: self.client.parse_response(self.create_response('404', 'Not Found')))
-
-if __name__ == '__main__':
-    unittest.main()
+    def APIError_test(self):
+        self.assert_not_raise(pyblr.APIError, lambda: self.client.parse_response(self.create_response('200', 'OK')))
+        self.assert_not_raise(pyblr.APIError, lambda: self.client.parse_response(self.create_response('300', 'Found')))
+        assert_raises(pyblr.APIError, lambda: self.client.parse_response(self.create_response('401', 'Not Authorized')))
+        assert_raises(pyblr.APIError, lambda: self.client.parse_response(self.create_response('404', 'Not Found')))
